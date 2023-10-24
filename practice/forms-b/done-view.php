daa324f18abe5d3186898781df58ff216d0ccc13
Minh Tran <mnt4en@virginia.edu>
2023-10-24 01:45:35 +0000

    garbage practice changes; unsure cancel correctly

---
+++ practice/forms-b/done-view.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+
+    <title>results</title>
+    <meta charset='utf-8'>
+
+</head>
+
+<body>
+
+    <h1>Results</h1>
+
+    <p>
+
+    </p>
+
+</body>
+
+</html>
